d166218b8f653b3b319427b192ca081d44707801
Haiaka <tttrewre21@gmail.com>
2026-08-29 23:35:43 +0800

    Site updated: 2026-08-29 23:35:42

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1788017537610";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1788017735211";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
